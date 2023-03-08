71c12ad06c419e308751d396f31db5c46bc9696d
Sreeram-AVX <121025285+Sreeram-AVX@users.noreply.github.com>
2023-03-08 15:40:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ resource "aws_vpn_gateway_route_propagation" "customer-vpc" {
    depends_on = [
     aviatrix_vpc.spoke-transit-vpcs,aws_vpn_gateway.vpn_gateway
   ]
-  count = length(data.aviatrix_vpc.route_tables)
+  count = length(data.aviatrix_vpc.test.route_tables)
   vpn_gateway_id = aws_vpn_gateway.vpn_gateway.id
   route_table_id = aviatrix_vpc.spoke-transit-vpcs[0].route_tables[count.index]
 }
